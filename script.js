be4cfa172f8e31dfdc966b4ad95e00c342d27f12
grillgrut <grillgrut@gmail.com>
2026-04-23 08:36:19 -0500

    Create the function for the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+function getComputerChoice(){
+    let choice = Math.ceil(Math.random() * 3)
+    if (choice === 3)
+        {
+            console.log(choice);
+            return "Paper";
+        }
+    else if (choice === 2)
+        {
+            console.log(choice);
+            return "Rock";
+        }
+    else 
+        {
+            console.log(choice);   
+            return "Scissors";
+        }
+    
+}
+
+console.log(getComputerChoice());
