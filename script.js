cb8c634de0d61d3d7d9db897d431688826b17577
grillgrut <grillgrut@gmail.com>
2026-04-23 09:06:43 -0500

    Get human choice and save it to a variable

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 function getComputerChoice(){
     let choice = Math.ceil(Math.random() * 3)
+
     if (choice === 3)
         {
-            console.log(choice);
             return "Paper";
         }
+
     else if (choice === 2)
         {
-            console.log(choice);
             return "Rock";
         }
+
     else 
         {
-            console.log(choice);   
             return "Scissors";
         }
     
 }
 
-console.log(getComputerChoice());
+function getHumanChoice()
+    {
+        let answer = prompt("Rock, Paper, Scissors?");
+        return answer;
+    }
+
+ 
+
+
+console.log(getHumanChoice());
+
